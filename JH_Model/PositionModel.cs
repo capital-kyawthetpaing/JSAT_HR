88baa19932f0308e8c1fb43c78dda293a7415f6c
capital-yinmonoo <60595809+capital-yinmonoo@users.noreply.github.com>
2020-02-12 14:13:27 +0630

    by amon

---
+++ JH_Model/PositionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JH_Model
+{
+    public class PositionModel
+    {
+        public string PostitionCD { get; set; }
+        
+        public string PositionName { get; set; }
+    }
+}
