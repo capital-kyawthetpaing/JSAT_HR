f1748223379aa69d67b5e9ffcecdac4934ed0381
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2020-02-12 10:50:06 +0630

    by pann nu

---
+++ Position_BL/PositionBL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using JH_DL;
+using System.Data.SqlClient;
+
+namespace Position_BL
+{
+    public class PositionBL
+    {
+        public DataTable GETPosition()
+        {
+            BaseDL bdl = new BaseDL();
+            SqlParameter[] prms = new SqlParameter[0];
+            return bdl.SelectData("M_Position_SelectAll", prms);
+        }
+    }
+}
