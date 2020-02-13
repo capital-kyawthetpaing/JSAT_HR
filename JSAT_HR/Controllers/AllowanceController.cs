f5364237ee00477d75c66dd4a400e15f000befe0
capital-yinmonoo <60595809+capital-yinmonoo@users.noreply.github.com>
2020-02-13 09:46:08 +0630

    by amon

---
+++ JSAT_HR/Controllers/AllowanceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JSAT_HR.Controllers
+{
+    public class AllowanceController : Controller
+    {
+        // GET: Allowance
+        public ActionResult Allowance_Setting()
+        {
+            return View();
+        }
+    }
+}
