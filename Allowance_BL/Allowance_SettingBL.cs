aaff65ac1ebe2980828716a7ebbd96715511bdac
capital-yinmonoo <60595809+capital-yinmonoo@users.noreply.github.com>
2020-02-13 11:27:15 +0630

    by amon

--- Allowance_BL/Allowance_SettingBL.cs
+++ Allowance_BL/Allowance_SettingBL.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JH_Model;
+using JH_DL;
+using System.Data.Entity;
 
 namespace Allowance_BL
 {
     public class Allowance_SettingBL
     {
+
+        public string Allowance_Setting_Save(AllowanceModel am)
+        {
+            //string msg = string.Empty;
+            //JSAT_HREntities db = new JSAT_HREntities();
+            //M_Allowance tb = new M_Allowance();
+
+            //tb.MD = am.MD;
+            //tb.Manager = am.Manager;
+            //tb.Director = am.Director;
+            //tb.N1 = am.N1;
+            //tb.N2 = am.N2;
+            //tb.N3 = am.N3;
+            //tb.JpnUniGrade = am.JpnUniGrade;
+            
+           
+
+
+
+
+            //return msg;
+            
+
+        }
     }
 }
