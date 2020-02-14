a4203dc62c02e908af64a69e17aa1a8a0ccea20c
Saw Yupar Kyaw <capital-sawyuparkyaw>
2020-02-14 15:10:49 +0630

    Company_List by Saw

--- Company_BL/CompanyBL.cs
+++ Company_BL/CompanyBL.cs
@@ -5,11 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using JH_DL;
 using JH_Model;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Company_BL
 {
     public class CompanyBL
     {
-
+        public DataTable GETCompany()
+        {
+            BaseDL bdl = new BaseDL();
+            SqlParameter[] prms = new SqlParameter[0];
+            return bdl.SelectData("M_Company_SelectAll", prms);
+        }
     }
 }
---
+++ JH_Model/CompanyModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JH_Model
+{
+    public class CompanyModel
+    {
+        public string CompanyCD { get; set; }
+
+        public string CompanyName { get; set; }
+    }
+}
