132c34b184a70ca0f57a8aa8a0046ec4c21a52c3
capital-yinmonoo <60595809+capital-yinmonoo@users.noreply.github.com>
2020-02-14 15:39:45 +0630

    by amon

--- Office_BL/OfficeBL.cs
+++ Office_BL/OfficeBL.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Entity;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using JH_DL;
+using JH_Model;
+
 
 namespace Office_BL
 {
@@ -16,6 +20,31 @@ namespace Office_BL
             BaseDL bdl = new BaseDL();
             SqlParameter[] prms = new SqlParameter[0];
             return bdl.SelectData("M_Office_SelectAll", prms);
+        }
+
+        public async Task<string> Hours_Setting_Save(OfficeModel am)
+        {
+
+            string msg = string.Empty;
+            JSAT_HREntities db = new JSAT_HREntities();
+            
+
+            M_Office tb = await db.M_Office.Where(s => s.OfficeCD.Equals("1")).SingleOrDefaultAsync();
+            tb.OfficeHourFrom = TimeSpan.Parse(am.OfficeTimeIn);
+            tb.OfficeHourTo = TimeSpan.Parse(am.OfficeTimeOut);
+            db.M_Office.Add(tb);
+            db.SaveChanges();
+
+            M_Office tb1 = await db.M_Office.Where(s => s.OfficeCD.Equals("2")).SingleOrDefaultAsync();
+            tb1.OfficeHourFrom = TimeSpan.Parse(am.AcademyTimeIn);
+            tb1.OfficeHourTo = TimeSpan.Parse(am.AcademyTimeOut);
+            db.M_Office.Add(tb1);
+            db.SaveChanges();
+
+            msg = "OK";
+            return msg;
+
+
         }
     }
 }
