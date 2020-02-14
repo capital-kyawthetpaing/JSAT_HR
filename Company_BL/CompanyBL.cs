565beb631d3459b137237ca8e81bd5d6703f9766
Saw Yupar Kyaw <capital-sawyuparkyaw>
2020-02-14 14:00:54 +0630

    add Company_list by saw

---
+++ Company_BL/CompanyBL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JH_DL;
+using JH_Model;
+
+namespace Company_BL
+{
+    public class CompanyBL
+    {
+
+    }
+}
