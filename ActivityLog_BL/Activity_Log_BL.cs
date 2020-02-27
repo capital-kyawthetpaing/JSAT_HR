0ae157bf8b9433699e93d0d59ba4a89881268eab
Saw Yupar Kyaw <capital-sawyuparkyaw>
2020-02-27 17:34:03 +0630

    By SawLay

--- ActivityLog_BL/Activity_Log_BL.cs
+++ ActivityLog_BL/Activity_Log_BL.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 using System.Web;
 using JH_DL;
 using JH_Model;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ActivityLog_BL
 {
     public class Activity_Log_BL
     {
+        public DataTable GETL_Log()
+        {
+            BaseDL bdl = new BaseDL();
+            SqlParameter[] prms = new SqlParameter[0];
+            return bdl.SelectData("M_Company_SelectAll", prms);
+        }
     }
 }
