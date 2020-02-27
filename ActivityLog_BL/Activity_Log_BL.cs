de41c79b6df126cfb57db2837eb8c0078abd9eda
Saw Yupar Kyaw <capital-sawyuparkyaw>
2020-02-27 16:31:41 +0630

    By SawLay

---
+++ ActivityLog_BL/Activity_Log_BL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using JH_DL;
+using JH_Model;
+
+namespace ActivityLog_BL
+{
+    public class Activity_Log_BL
+    {
+    }
+}
