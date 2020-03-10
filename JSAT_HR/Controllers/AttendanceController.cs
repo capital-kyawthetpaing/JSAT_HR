8dee438b41f5cb527b228ae7e5897289067e1e3d
Ay Mi Soe <Ay Mi Soe@DESKTOP-DJ5ADD9>
2020-03-10 12:54:05 +0630

    byami

--- JSAT_HR/Controllers/AttendanceController.cs
+++ JSAT_HR/Controllers/AttendanceController.cs
@@ -247,13 +247,8 @@ namespace JSAT_HR.Controllers
         }
 
         public ActionResult QuickAttendance()
-        {
-            MultiModel model = new MultiModel();
-            model.DayList = new SelectList(new List<string>() { "1", "2", "3","4","5", "6", "7", "8", "9", "10",
-                                                                  "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
-                                                                  "21", "22", "23", "24", "25", "26", "27", "28", "29", "30" });
-            ViewBag.data = model.DayList;
-            return View(model);
+        {           
+            return View();
         }
 
     }
